de29a50b2117546bd8259f68fb216d338db5c964
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-08 15:14:24 +0200

    Push exercice-4.py QF

--- NSI-Terminale-2021-2022/recursivity/exercice-4.py
+++ NSI-Terminale-2021-2022/recursivity/exercice-4.py
@@ -1,16 +1,25 @@
 import turtle as t
+from random import randint as rn
 
-def Arbre(longueur=100):
+colors = ("blue", "red", "green", "yellow", "brown", "pink", "orange", "purple") 
+
+def Arbre(longueur=100, steps=1):
     if longueur < 10: return
 
+    t.color(colors[rn(0, len(colors) - 1)])
+    t.width(5/steps)
     t.fd(longueur)
-    t.left(30)
+    t.left(45)
     Arbre(longueur * 0.6)
-    t.right(60)
-    Arbre(longueur * 0.6)
-    t.left(30)
+    t.right(90)
+    steps+=1
+    Arbre(longueur * 0.6, steps)
+    t.left(45)
     t.fd(-longueur)
     return
 
-Arbre(200)
+t.setheading(90)
+t.speed(0)
+t.setposition(0, -300)
+Arbre(300)
 t.done()
