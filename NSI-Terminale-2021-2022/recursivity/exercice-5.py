fa6a5c8024b736567f0e8bd166645764f9038bb2
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-16 14:22:24 +0200

    Push exercice-5.py

--- NSI-Terminale-2021-2022/recursivity/exercice-5.py
+++ NSI-Terminale-2021-2022/recursivity/exercice-5.py
@@ -1,8 +1,17 @@
 def echiquier(n):
-    if n == 0: return 0
+    if n == 0: return 1
     return echiquier(n - 1) + 2**n
 
 def calc_max_weight(n):
     return (echiquier(n) * 0.04) / 1000
 
-print(f"{(calc_max_weight(63) / 1000) / 450000000 }y")
+def echiquier2(n):
+    if n == 0: 
+        print(f"Total\t2**n\n1\t1")
+        return 1
+    result = echiquier2(n - 1) + 2**n
+    print(f"{result}\t{2**n}")
+    return result
+
+# print(f"{(calc_max_weight(63) / 1000) / 450000000 }y")
+echiquier2(63)
