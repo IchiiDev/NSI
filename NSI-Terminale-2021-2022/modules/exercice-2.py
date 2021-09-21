5049578f7dba9f7ec30267dfff377ea3ced04d9f
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-21 10:28:38 +0200

    Push of exercice-2.py

---
+++ NSI-Terminale-2021-2022/modules/exercice-2.py
@@ -0,0 +1,7 @@
+import geometry
+
+# Q2
+print(geometry.aireTrapese(10, 8, 5))
+
+# Q3
+print(geometry.aireDisque(2.5))
--- NSI-Terminale-2021-2022/modules/geometry.py
+++ NSI-Terminale-2021-2022/modules/geometry.py
@@ -1,5 +1,13 @@
+from math import pi
+
 def aireCarre(x):
     return x * x
 
 def aireTriangle(b, h):
     return (b * h) / 2
+
+def aireTrapese(B, b, h):
+    return ((B + b) * h) / 2
+
+def aireDisque(r):
+    return (r**2) * pi
