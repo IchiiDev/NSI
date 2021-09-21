c69152cdfe5d8ae13c54e3b175b17e37e97dfe91
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-21 10:40:40 +0200

    Push exercice-5.py

---
+++ NSI-Terminale-2021-2022/modules/exercice-4.py
@@ -0,0 +1,7 @@
+import geometry as aire
+
+print(aire.aireCarre(5))
+print(aire.aireTriangle(2, 5))
+print(aire.aireCarre(10) + aire.aireTriangle(10, 10))
+print(aire.aireTrapese(10, 8, 5))
+print(aire.aireDisque(2.5))
