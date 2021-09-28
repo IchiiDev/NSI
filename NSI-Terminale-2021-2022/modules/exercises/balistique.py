7474aa5840108c5e5a85964377f904e47d30eb1e
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-28 10:51:14 +0200

    Push exercice-2.py

--- NSI-Terminale-2021-2022/modules/exercises/balistique.py
+++ NSI-Terminale-2021-2022/modules/exercises/balistique.py
@@ -1,4 +1,7 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+import turtle as t
 
 class projectile:
 
@@ -18,3 +21,42 @@ class projectile:
 
     def getMaxHeight(self):
         return (self.v**2 * math.sin(self.alpha)**2) / 2 ** self.G
+
+    def getY(self, x):
+        alpha = self.alpha * math.pi / 180
+        return -1 / 2 * self.G * x ** 2 / ((math.cos(alpha))**2 * self.v ** 2) + math.tan(alpha) * x * self.h
+
+
+    def tracerTrajectoire(self, xmax, n):
+        """
+        Représenter la trajectoire
+        xmax : valeur maximale pour tracer la courbe
+        n : nombre de points sur la courbe
+        """
+        liste_X = np.linspace(0, xmax, n)
+        liste_Y = [ self.getY(x) for x in liste_X]
+        plt.figure()
+        plt.plot(liste_X, liste_Y)
+        plt.grid()
+        plt.xlabel("Distance d")
+        plt.ylabel("Hauteur y")
+        plt.legend()
+        plt.show()
+                
+    def animerTrajectoire(self, xmax, n, e):
+        """
+        Animer la trajectoire
+        xmax : valeur maximale pour tracer la courbe
+        n : nombre de points sur la courbe
+        e : coefficient de mise à l'échelle
+        """
+        liste_X = np.linspace(0, xmax, n)
+        liste_Y = [ self.getY(x) for x in liste_X]
+        t.speed("slowest")
+        t.up()
+        t.goto(liste_X[0] * e, liste_Y[0] * e)
+        t.down()
+        for k in range(1, n):
+            t.goto(liste_X[k] * e, liste_Y[k] * e)
+        t.done()
+
--- NSI-Terminale-2021-2022/modules/exercises/exercice-2.py
+++ NSI-Terminale-2021-2022/modules/exercises/exercice-2.py
@@ -1,7 +1,10 @@
 from balistique import projectile
 
-trajectoire = projectile(45, 20, 0)
+trajectoire = projectile(65, 13, 2)
 
 print(trajectoire.getDistance())
 print(trajectoire.getFlightTime())
 print(trajectoire.getMaxHeight())
+print(trajectoire.getY(10))
+trajectoire.tracerTrajectoire(15, 20)
+trajectoire.animerTrajectoire(15, 20, 20)
