fd3b363d9c93bcd8e2f088118e40d162f0d63e76
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-28 11:27:40 +0200

    push exercice-3.py

---
+++ NSI-Terminale-2021-2022/object-oriented/exercice3.py
@@ -0,0 +1,15 @@
+class Velo:
+
+    def __init__(self):
+        self.__couleur = "Rouge"
+        self.__roues = 2
+        self.__pos = 0
+
+    def obtenir_pos(self):
+        return self.__pos
+
+    def modifier_pos(self, pos):
+        self.__pos = pos
+
+    def avancer(self, dis):
+        self.__pos += dis
