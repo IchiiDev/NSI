2dee5dc73acc96fe3f7eb971623d15e1b115087c
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-10-12 10:59:40 +0200

    hell.

--- NSI-Terminale-2021-2022/object-oriented/exercises/pysimplegui.py
+++ NSI-Terminale-2021-2022/object-oriented/exercises/pysimplegui.py
@@ -1,6 +1,8 @@
 #import PySimpleGUIWeb as sg
 import PySimpleGUI as sg
 from random import randint
+
+from PySimpleGUI.PySimpleGUI import button_color_to_tuple
 # ---------------------------- Fonctions et classes --------------------------------------
 class IndividuT1():    
     
@@ -11,19 +13,19 @@ class IndividuT1():
         self.couleurTexte = couleurTexte        
         self.couleurFond = couleurFond    
         
-    def deplacerHorizontal(self, dis):        
+    def deplacerHorizontal(self, dis, individuAutre):        
         nextX = self.x + dis
-        if 0 <= nextX > MAX_COL:
+        if 0 <= nextX < MAX_COL-1:
             self.x = nextX
         else:
-            self.x = MAX_COL if nextX >= 0 else 0
+            self.x = MAX_COL-1 if nextX >= 0 else 0
 
     def deplacerVertical(self, dis):
         nextY = self.y + dis
-        if 0 <= nextY > MAX_ROWS:
+        if 0 <= nextY < MAX_ROWS-1:
             self.y = nextY
         else:
-            self.y = MAX_ROWS if nextY >= 0 else 0
+            self.y = MAX_ROWS-1 if nextY >= 0 else 0
 
 # -------------- Programme principal - Appel des fonctions -------------------------------------
 
@@ -31,7 +33,7 @@ class IndividuT1():
 
 # -- Objets de type Individu --
 individu1 = IndividuT1(0, 0,'+', 'white','purple') # (x, y, label, couleurTexte, couleurFond)
-# individu2 = IndividuT1(0, 0, "-", "white", 'green')
+individu2 = IndividuT1(0, 0, "-", "white", 'green')
 # -- Paramètres de l'inteface graphique --
 MAX_ROWS = MAX_COL = 10
 board = [[randint(0,1) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
@@ -39,6 +41,7 @@ layout =  [[sg.Button('?', size=(1, 1), key=(i,j), pad=(0,0)) for j in range(MAX
 window = sg.Window('Déplacer objets de type cellule', layout)
 
 # ---- Boucle principale GUI ----
+i, j, k, l = (0, 0, 0, 0)
 while True:    
     event, values = window.read(timeout=100)    
     if event in (sg.WIN_CLOSED, 'Exit'):        
@@ -47,18 +50,31 @@ while True:
     # window[(row, col)].update('New text')  
     # To change a button's text, use this pattern    
     # For this example, change the text of the button to the board's value and turn color black        
+    window[(i, j)].update("?", button_color=("white", "#283b5b"))
+    window[(k, l)].update("?", button_color=("white", "#283b5b"))
     i = individu1.y    
-    j = individu1.x    
+    j = individu1.x
+      
     label = individu1.label    
     couleurTexte = individu1.couleurTexte   
     couleurFond = individu1.couleurFond   
-    window[(i, j)].update(label, button_color=(couleurTexte,couleurFond)) 
+    window[(i, j)].update(label, button_color=(couleurTexte,couleurFond))
 
-    x = randint(-1, 1)
-    y = randint(-1, 1)
-    print(x, y)
-    individu1.deplacerVertical(y)
-    individu1.deplacerHorizontal(x)
+    label = individu2.label    
+    couleurTexte = individu2.couleurTexte   
+    couleurFond = individu2.couleurFond 
+    k = individu2.y
+    l = individu2.y
+    window[(k, l)].update(label, button_color=(couleurTexte,couleurFond))
+
+    x1 = randint(-1, 1)
+    y1 = randint(-1, 1)
+    x2 = randint(-1, 1)
+    y2 = randint(-1, 1)
+    individu1.deplacerVertical(y1, individu2)
+    individu1.deplacerHorizontal(x1, individu2)
+    individu2.deplacerVertical(y2, individu1)
+    individu2.deplacerHorizontal(y2, individu1)
 
 # ---- Fin / Fermer fenêtre graphique ----
 window.close()
